0a7118c306c8f4f3645342ab346db0c25070c23d
Kamil Ambroży <62301012+Kamileoo@users.noreply.github.com>
2022-01-12 00:08:57 +0100

    Key options

--- Pong/game.py
+++ Pong/game.py
@@ -2,9 +2,13 @@ from PyQt6.QtWidgets import QApplication, QWidget, QGraphicsEllipseItem, QPushBu
 from PyQt6 import QtCore, QtGui, QtWidgets
 import sys
 from main import *
+import main
 import hub
+import time
 
-# min 0 max 512-line_len=352
+
+threads = []
+
 
 game_widget = {
     'startbutton': [],
@@ -13,35 +17,56 @@ game_widget = {
     'enscore': [],
     'myline': [],
     'enline': [],
-    'ball': []
+    'ball': [],
+    'result': []
 }
 
 game_val = {
-    'my_pos': 0,
-    'en_pos': 0,
-    'ball_x': 0,
-    'ball_y': 0,
+    'my_pos': 0,    # 0 - 100
+    'en_pos': 0,    # 0 - 100
+    'ball_x': 0,    # 0 - 200
+    'ball_y': 0,    # 0 - 100
     'my_score': 0,
-    'en_score': 0
+    'en_score': 0,
+    'is_game': 0,
+    'key': 0,
+    'esckey': 0
 }
 
 game_opt = {
     'line_len': 160,
     'line_wid': 20,
-    'ball_rad': 10
+    'ball_rad': 10,
+    'from_border': 10
 }
 
 
+# When Press Start
 def is_ready():
     game_widget['startbutton'][-1].hide()
+    game_val['is_game'] = 1
     game()
 
 
+# When Press End
 def is_end(win):
     game_clear(win)
     hub.hub_ui(win)
 
 
+# After the Game Ends
+def game_is_over():
+    game_val['is_game'] = 0
+    game_widget['endbutton'][-1].show()
+    game_widget['ball'][-1].hide()
+    res = 'You Won!' if game_val['my_score'] > game_val['en_score']\
+        else 'You Lost!' if game_val['my_score'] < game_val['en_score'] \
+        else 'It is a Tie'
+    game_widget['result'][-1].setText(res)
+    game_widget['result'][-1].show()
+
+
+# Clearing the window
 def game_clear(win):
     win.setStyleSheet("background-color: #ffffff;")
     for h in game_widget:
@@ -53,17 +78,60 @@ def game_clear(win):
         game_val[v] = 0
 
 
+# Keyboard
+def keyHandler(event, status):
+    if status == 'P':
+        if not event.isAutoRepeat():
+            if event.key() == QtCore.Qt.Key.Key_W:
+                game_val['key'] = 1
+            elif event.key() == QtCore.Qt.Key.Key_S:
+                game_val['key'] = -1
+            elif event.key() == QtCore.Qt.Key.Key_Escape:
+                game_val['esckey'] = 1
+    elif status == 'R':
+        if not event.isAutoRepeat():
+            game_val['key'] = 0
+        pass
+    else:
+        pass
+
+
+# Drawing Palettes, scores and ball
+def draw():
+    game_widget['myscore'][-1].setText(str(game_val['my_score']))
+    game_widget['myscore'][-1].show()
+
+    game_widget['enscore'][-1].setText(str(game_val['en_score']))
+    game_widget['enscore'][-1].show()
+
+    game_widget['ball'][-1].setGeometry(QtCore.QRect((main.glob_params['windows_width'] - 2 * game_opt['ball_rad']) * game_val['ball_x'] / 200,
+                                                     (main.glob_params['windows_height'] - 2 * game_opt['ball_rad']) * game_val['ball_y'] / 100,
+                                                     2 * game_opt['ball_rad'], 2 * game_opt['ball_rad']))
+    game_widget['ball'][-1].show()
+
+    game_widget['myline'][-1].setGeometry(QtCore.QRect(game_opt['from_border'],
+                                                    (main.glob_params['windows_height'] - game_opt['line_len']) * game_val['my_pos'] / 100,
+                                                    game_opt['line_wid'], game_opt['line_len']))
+    game_widget['myline'][-1].show()
+
+    game_widget['enline'][-1].setGeometry(QtCore.QRect(main.glob_params['windows_width']-game_opt['from_border']-game_opt['line_wid'],
+                                                       (main.glob_params['windows_height'] - game_opt['line_len']) * game_val['en_pos'] / 100,
+                                                       game_opt['line_wid'], game_opt['line_len']))
+    game_widget['enline'][-1].show()
+
+
+# Game
 def game():
-    #while 1:
-    #    game_widget['myscore'][-1].setText[str(game_val['my_score'])]
-    #    game_widget['enscore'][-1].setText[str(game_val['en_score'])]
-    game_widget['endbutton'][-1].show()
+    #game_is_over()
     pass
 
 
+# Drawing and setting the window
 def game_ui(win):
-    game_val['my_pos'] = (512 - game_opt['line_len']) / 2
-    game_val['en_pos'] = (512 - game_opt['line_len']) / 2
+    game_val['my_pos'] = 50
+    game_val['en_pos'] = 50
+    game_val['ball_x'] = 100
+    game_val['ball_y'] = 50
 
     win.setStyleSheet("background-color: #363636;")
 
@@ -88,7 +156,6 @@ def game_ui(win):
     myscore.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
     myscore.setGeometry(QtCore.QRect(462, 0, 35, 30))
     game_widget['myscore'].append(myscore)
-    game_widget['myscore'][-1].show()
 
     # Enemy score
     enscore = QtWidgets.QLabel(str(game_val['en_score']), win)
@@ -100,7 +167,6 @@ def game_ui(win):
     enscore.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
     enscore.setGeometry(QtCore.QRect(527, 0, 35, 30))
     game_widget['enscore'].append(enscore)
-    game_widget['enscore'][-1].show()
 
     # End button
     endbutton = QtWidgets.QPushButton("End", win)
@@ -113,32 +179,47 @@ def game_ui(win):
     game_widget['endbutton'].append(endbutton)
     game_widget['endbutton'][-1].hide()
 
-    #ball = QGraphicsEllipseItem()
-    #ball.setRect(0,0,5,5)
-
+    # My Palette
     my_line = QtWidgets.QFrame(win)
-    my_line.setGeometry(QtCore.QRect(10, game_val['my_pos'], game_opt['line_wid'], game_opt['line_len']))
     my_line.setLineWidth(0)
     my_line.setMidLineWidth(game_opt['line_wid'])
     my_line.setFrameShape(QtWidgets.QFrame.Shape.VLine)
     my_line.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
     game_widget['myline'].append(my_line)
-    game_widget['myline'][-1].show()
 
+    # Enemy Palette
     en_line = QtWidgets.QFrame(win)
-    en_line.setGeometry(QtCore.QRect(1024-10-game_opt['line_wid'], game_val['en_pos'], game_opt['line_wid'], game_opt['line_len']))
     en_line.setLineWidth(0)
     en_line.setMidLineWidth(game_opt['line_wid'])
     en_line.setFrameShape(QtWidgets.QFrame.Shape.VLine)
     en_line.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
     game_widget['enline'].append(en_line)
-    game_widget['enline'][-1].show()
 
+    # Ball
     ball = QtWidgets.QLabel(win)
-    ball.setGeometry(QtCore.QRect((1024-2*game_opt['ball_rad'])/2, (512-2*game_opt['ball_rad'])/2, 2*game_opt['ball_rad'], 2*game_opt['ball_rad']))
     ball.setStyleSheet(
         "background: white;" +
         f"border-radius: {game_opt['ball_rad']}px;"
     )
     game_widget['ball'].append(ball)
-    game_widget['ball'][-1].show()
+
+    # Score text
+    endscore = QtWidgets.QLabel(win)
+    endscore.setStyleSheet(
+        "font-size: 40px;" +
+        "color: 'white';" +
+        "background: transparent;"
+    )
+    endscore.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+    endscore.setGeometry(QtCore.QRect(0,main.glob_params['windows_height']/4 , main.glob_params['windows_width'], 40))
+    game_widget['result'].append(endscore)
+    game_widget['result'][-1].hide()
+
+    # Draw Palettes, Ball and Scores
+    draw()
+
+# Game itself
+class myGame(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+    pass
--- Pong/main.py
+++ Pong/main.py
@@ -5,15 +5,29 @@ import game
 import hub
 
 
+glob_params = {
+    'windows_height': 512,
+    'windows_width': 1024,
+}
+
+
+class MyWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+
+    def keyPressEvent(self, event):
+        if game.game_val['is_game']:
+            game.keyHandler(event, 'P')
+    def keyReleaseEvent(self, event):
+        if game.game_val['is_game']:
+            game.keyHandler(event, 'R')
+
 app = QtWidgets.QApplication(sys.argv)
-win = QtWidgets.QWidget()
+win = MyWindow()
 win.setWindowTitle("Pong")
-#win.resize(1024, 512)
 win.setFixedSize(1024, 512)
 
-#game.game_ui(win)
 hub.hub_ui(win)
-
 
 win.show()
 sys.exit(app.exec())
