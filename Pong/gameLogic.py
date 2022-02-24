b901df2e92530b838b05f893d9f839a9c2d74833
Kamil Ambroży <62301012+Kamileoo@users.noreply.github.com>
2022-02-24 22:22:11 +0100

    Major UI changes

--- Pong/gameLogic.py
+++ Pong/gameLogic.py
@@ -1,23 +1,50 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QGraphicsEllipseItem, QPushButton
 from PyQt6 import QtCore, QtGui, QtWidgets
-import sys
-import main
+from PyQt6.QtNetwork import QTcpSocket, QAbstractSocket
+import config
 import game
-import time
 
 
 # Game itself
-class myGame(QtCore.QObject):
+class MyGame(QtCore.QObject):
 
     finished = QtCore.pyqtSignal()
 
     def run(self):
-        while not game.game_val['esckey']:
-            if game.game_val['key'] == 1 and game.game_val['my_pos_x'] <= game.game_opt['im_height']:
-                game.game_val['my_pos_x'] +=1
-            elif game.game_val['key'] == -1  and game.game_val['my_pos_x'] >= 0:
-                game.game_val['my_pos_x'] -=1
+        print("RUN")
+        socket = QTcpSocket()
+        socket.connected.connect(lambda: print("POlączono z serwerem"))
+        socket.disconnected.connect(lambda: print("Disconnected"))
+        ip=config.glob_params['ip']
+        port=int(config.glob_params['port'])
+        socket.connectToHost(ip, port, protocol=QAbstractSocket.NetworkLayerProtocol.IPv4Protocol)
+        socket.waitForConnected(-1)
+        socket.waitForReadyRead(-1)
+
+        game_val=game.game_val
+        base = list('00000')
+        while 1:
+            game_val = game.game_val
+            socket.waitForReadyRead(-1)
+            buff = socket.readLine(20).decode("ascii")
+            print(buff)
+            positions = [int(elem) for elem in buff.split(',')]
+            print(positions)
+            game_val['my_pos_x'] = positions[0]
+            game_val['en_pos_x'] = positions[1]
+            game_val['ball_x'] = positions[2]
+            game_val['ball_y'] = positions[3]
             game.draw()
-            time.sleep(0.001)
-        game.game_is_over()
-        self.finished.emit()
+            move = game_val['move_x']
+            if move != 0:
+                sign='+' if move>=0 else '-'
+                move=abs(move)
+                move_string=str(move)
+                offset=len(base)-len(move_string)-1
+                for idx, elem in enumerate(move_string):
+                    base[idx+offset]=elem
+                base[-1]=sign
+                print(''.join(base).encode('ascii'))
+                socket.write(''.join(base).encode('ascii'))
+                socket.waitForBytesWritten(-1)
+                game_val['move_x']=0
+                base = list('00000')
--- Pong/hub.py
+++ Pong/hub.py
@@ -1,21 +1,51 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QGraphicsEllipseItem, QPushButton
 from PyQt6 import QtCore, QtGui, QtWidgets
-import sys
-
-import main
-from main import *
 import game
-#from game import game_ui
+import config
+import account
+import loginHistory
 
 hub_widget = {
-    'grid': [],
     'startbutton': [],
-    'player': []
+    'player': [],
+    'guilds': [],
+    'achievements': [],
+    'statistics': [],
+    'loginhistory': [],
+    'gameshistory': [],
+    'account': [],
+    'spacers': []
 }
 
 def start_game(win):
     hub_clear()
     game.game_ui(win)
+
+
+def guilds():
+    pass
+
+
+def achievements():
+    pass
+
+
+def statistics():
+    pass
+
+def gameshistory():
+    pass
+
+
+def loginhistory():
+    hub_clear()
+    loginHistory.lg_ui()
+    pass
+
+
+
+def account_func():
+    hub_clear()
+    account.draw()
 
 
 def hub_clear():
@@ -24,31 +54,88 @@ def hub_clear():
             hub_widget[h][-1].deleteLater()
         for i in range(len(hub_widget[h])):
             hub_widget[h] = []
+    config.glob_grid.setColumnMinimumWidth(0, 0)
+    config.glob_grid.setRowStretch(2, 0)
+    config.glob_grid.setRowStretch(9, 0)
 
 
 def create_main_button(text):
     button = QtWidgets.QPushButton(text)
-    button.setStyleSheet(
-        "margin: 0 300px;"
-    )
-    #button.setObjectName(text)
+    # button.setStyleSheet(
+    #     "margin: 0px 10px;" +
+    #     "padding: 10px 10px;"
+    # )
 
     return button
 
 
 def hub_ui(win):
-    grid = QtWidgets.QGridLayout(win)
 
-    player = QtWidgets.QLabel("Player")
+    # Nickname
+    #player = QtWidgets.QLabel("Player")
+    player = QtWidgets.QLabel(config.login_params['username'])
     player.setMaximumHeight(20)
-    player.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+    player.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
     hub_widget['player'].append(player)
-    grid.addWidget(hub_widget['player'][-1], 0, 0, QtCore.Qt.AlignmentFlag.AlignTop)
+    hub_widget['player'][-1].show()
+    config.glob_grid.addWidget(hub_widget['player'][-1], 0, 2, QtCore.Qt.AlignmentFlag.AlignTop)
 
+    # Account
+    acc = QtWidgets.QPushButton("Account")
+    acc.setStyleSheet(
+        "margin: 0px 80px;"
+    )
+    acc.clicked.connect(account_func)
+    hub_widget['account'].append(acc)
+    hub_widget['account'][-1].show()
+    config.glob_grid.addWidget(hub_widget['account'][-1], 1, 2)
 
+    # Game start
     startb = create_main_button("Start")
     startb.clicked.connect(lambda: start_game(win))
     hub_widget['startbutton'].append(startb)
-    grid.addWidget(hub_widget['startbutton'][-1], 1, 0, QtCore.Qt.AlignmentFlag.AlignTop)
+    hub_widget['startbutton'][-1].show()
+    config.glob_grid.addWidget(hub_widget['startbutton'][-1], 3, 1)
 
-    hub_widget['grid'].append(grid)
+    # Guilds
+    gul = create_main_button("Guilds")
+    gul.clicked.connect(guilds)
+    hub_widget['guilds'].append(gul)
+    hub_widget['guilds'][-1].show()
+    config.glob_grid.addWidget(hub_widget['guilds'][-1], 4, 1)
+
+    # Achievements
+    ach = create_main_button("Achievements")
+    ach.clicked.connect(achievements)
+    hub_widget['achievements'].append(ach)
+    hub_widget['achievements'][-1].show()
+    config.glob_grid.addWidget(hub_widget['achievements'][-1], 5, 1)
+
+    # Statistics
+    stat = create_main_button("Statistics")
+    stat.clicked.connect(statistics)
+    hub_widget['statistics'].append(stat)
+    hub_widget['statistics'][-1].show()
+
+    config.glob_grid.addWidget(hub_widget['statistics'][-1], 6, 1)
+    # Games History
+    gam = create_main_button("Games History")
+    gam.clicked.connect(gameshistory)
+    hub_widget['gameshistory'].append(gam)
+    hub_widget['gameshistory'][-1].show()
+    config.glob_grid.addWidget(hub_widget['gameshistory'][-1], 7, 1)
+
+    # Login History
+    log = create_main_button("Login History")
+    log.clicked.connect(loginhistory)
+    hub_widget['loginhistory'].append(log)
+    if config.login_params['isAdmin']:
+        hub_widget['loginhistory'][-1].show()
+    else:
+        hub_widget['loginhistory'][-1].hide()
+    config.glob_grid.addWidget(hub_widget['loginhistory'][-1], 8, 1)
+
+    # Spacers
+    config.glob_grid.setColumnMinimumWidth(0,config.glob_params['windows_width']/3)
+    config.glob_grid.setRowStretch(2, 1)
+    config.glob_grid.setRowStretch(9, 1)
